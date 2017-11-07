a16dd0a71e77b3bc5f1a4daf085757422601af2a
unknown <honza.klu@gmail.com>
2017-11-07 07:20:35 +0100

    dditional changes to gen.py

--- libs/gen.py
+++ libs/gen.py
@@ -5,9 +5,29 @@ class Individual:
         self.x = x        
         
 class GanAlg:
-    def __init__(self, fit_fun=None, area=(-1, 1, -1, 1)):
+    def __init__(self, fit_fun=None, area=(-1, 1, -1, 1), individuals=None, plot_size=None):
         self.fit_fun = fit_fun        
+        self.area = area
+        if individuals:
+            self.individuals = individuals
+        else:
+            self.individuals = []
+        self.plot_size = plot_size
+            
+    def _update_fit_landscape(self):
+        X = np.arange(self.area[0], self.area[1], 0.25)
+        Y = np.arange(self.area[2], self.area[3], 0.25)
+        X, Y = np.meshgrid(X, Y)
+        Z = self.fit_fun(X, Y)
+        self.land = Z
     def _draw_fit(self, ax):
-        ax.imshow(Z, extent=(LOW_LIMIT, HIGH_LIMIT, LOW_LIMIT, HIGH_LIMIT))
+        self._update_fit_landscape()
+        ax.imshow(self.land, extent=(self.area[0], self.area[1], self.area[2], self.area[3]))
     def _draw_ind(self, ax):
-        pass
+        self._update_fit_landscape()
+        x0 = [x.x[0] for x in self.individuals]
+        x1 = [x.x[1] for x in self.individuals]
+        ax.plot(x0, x1, 'x', color='r')
+        ax.autoscale(False)
+        self._draw_fit(ax)
+        
